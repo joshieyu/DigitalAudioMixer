ea91d750e0f1c7955a816f23a94355c43210b1cb
junidy <junidyeom@gmail.com>
2024-09-16 23:44:24 -0400

    Barebones website done

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,10 +1,51 @@
 import { useState } from 'react'
-import { Button, MantineProvider } from '@mantine/core';
+import { Button, Image, MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
+import ucfLogo from '/src/assets/ucfLogo.png';
+import dncDocument from '/src/assets/dncDocument.pdf';
 
 function App() {
   return (
     <MantineProvider>
+      <div className="w-3/4 m-auto flex items-center flex-col">
+        <h1>AI-Powered Digital Audio Mixer</h1>
+        <h2>"MISTER MIXER"</h2>
+        <Image className="w-24" src={ucfLogo} alt="UCF Logo"/>
+        <h2>ECE Senior Design – Group 14</h2>
+        <hr className="w-full" />
+        <div>
+          <h2>Description</h2>
+          <p>We aim to create a low-cost audio mixer that integrates cutting-edge techniques from audio engineering, offering capabilities not found in current devices. This project will deepen our understanding of audio engineering and result in a practical, high-performance product. By combining general computing with mixers, MISTER MIXER will empower musicians to produce a powerful device at a fraction of the cost of existing consumer options.</p>
+        </div>
+        <hr className="w-full" />
+        <div>
+          <h2>Documents</h2>
+          <Button component="a" href={dncDocument} target="_blank">Divide and Conquer (pending approval)</Button>
+        </div>
+        <hr className="w-full" />
+        <div>
+          <h3>Committee Members</h3>
+          <ul>
+            <li>John Aedo</li>
+            <li>Mark Maddox</li>
+            <li>Saleem Sahawneh</li>
+            <li>Mikhael Wasfy</li>
+            <li>Enxia Zhang</li>
+          </ul>
+        </div>
+        <hr className="w-full" />
+        <div>
+          <h3>Team Members</h3>
+          <ul>
+            <li>Juni Yeom, Computer Science</li>
+            <li>Joshua Yu, Computer Engineering</li>
+            <li>Michael Meyers, Electrical Engineering</li>
+            <li>Kevin Kurian, Computer Engineering</li>
+            <li>Srinilai Kankipati, Computer Engineering</li>
+          </ul>
+        </div>
+        <p className="text-gray-500 text-sm">Website under construction. Product under development. Direct all sales questions to sales@maestro.mix</p>
+      </div>
     </MantineProvider>
   )
 }
--- website/vite.config.js
+++ website/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/seniordesign/fa2024sp2025/g14/',
   plugins: [react()],
 })
